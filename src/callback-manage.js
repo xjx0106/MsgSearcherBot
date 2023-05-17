044547aa1f997329c8e1f77278a253f864f749e3
許景欣 <xjx0106@gmail.com>
2023-05-17 18:47:37 +0800

    perf: 翻頁按鈕和展示的優化

--- src/callback-manage.js
+++ src/callback-manage.js
@@ -9,7 +9,7 @@ const {
 } = require("./seacher")
 
 
-module.exports = bot.on("callback_query", onLoveText = (msg) => {
+module.exports = bot.on("callback_query", onLoveText = async (msg) => {
     const data = msg.data;
     const messageId = msg.message.message_id;
 
@@ -20,6 +20,12 @@ module.exports = bot.on("callback_query", onLoveText = (msg) => {
         s: size,
         t: type
     } = dataObj; // 從json對象裏解構出c、p、s、t，并且重命名為chatId、page、size、type
+
+    const tempMsgListData = await getData("tempMsgList"); // 包含了目標搜索詞的、臨時儲存著的消息
+    const {
+        list: msgsWithTarget,
+    } = tempMsgListData;
+    const totalPage = Math.ceil(msgsWithTarget.length / size);
 
     if (type === "pre") {
         // 上一頁
@@ -32,10 +38,12 @@ module.exports = bot.on("callback_query", onLoveText = (msg) => {
         }
     } else if (type === 'nex') {
         // 下一頁
-        const newPage = page + 1;
-        queryTempMsg(chatId, messageId, {
-            page: newPage,
-            size
-        });
+        if (page < totalPage) {
+            const newPage = page + 1;
+            queryTempMsg(chatId, messageId, {
+                page: newPage,
+                size
+            });
+        }
     }
 });
--- src/seacher.js
+++ src/seacher.js
@@ -70,17 +70,22 @@ const queryTempMsg = async (chatId, messageId = "no_messageId", param = {
 	const msgToDisplay = msgsWithTarget.slice((page - 1) * size, page * size);
 
 	let content = '';
-	msgToDisplay.forEach(item => {
+	msgToDisplay.forEach((item, index) => {
 		/**
 		 * 單條記錄要展示的行
 		 */
-		const line = "[" + item.from + "]:" + item.text_entities.map(ent => ent.text).join(' ') + "--------" + item.date;
+		const line = "【" + ((page - 1) * size + (index + 1)) + "】 [" + item.from + "]:" + item.text_entities.map(ent => ent.text).join(' ') + " -------- " + item.date;
 		content += line + '\n\n';
 	});
 
 	content = content.split(text).join("<b>" + text + "</b>");
 
-	content += '-----------------\n共有 ' + msgsWithTarget.length + " 條, 當前是第 " + page + " 頁，每頁展示 " + size + " 條";
+	const totalCount = msgsWithTarget.length;
+	const pagesLine = generatePageNumberList(page, size, totalCount);
+	content += pagesLine;
+
+	const totalPage = Math.ceil(msgsWithTarget.length / size);
+	content += '\n=============================\n共有 ' + msgsWithTarget.length + " 條, 頁數 " + page + " / " + totalPage + " 頁，每頁展示 " + size + " 條";
 
 
 	const callbackData1 = {
@@ -125,6 +130,10 @@ const queryTempMsg = async (chatId, messageId = "no_messageId", param = {
 			callback_data: cbdN,
 		},
 		{
+			text: " ",
+			callback_data: cbdN,
+		},
+		{
 			text: "-> 向后",
 			callback_data: cbd2,
 		}
@@ -153,6 +162,46 @@ const queryTempMsg = async (chatId, messageId = "no_messageId", param = {
 			}
 		});
 	}
+}
+
+/**
+ * 生成頁碼行字符串，如“1  2  3  4  5  6  7”等
+ * @param {*} page 當前頁數
+ * @param {*} size 單頁大小
+ * @param {*} totalCount 總條數
+ */
+const generatePageNumberList = (page, size, totalCount) => {
+	// \n1   2   3   4   <b><u>5</u></b>   6   7   8
+	let result = "";
+	const totalPage = Math.ceil(totalCount / size); // 縂頁數。看一共有幾頁
+	let pageNumberList = []; // totalPage為6的時候：['1','2','3','4','5','6']
+	for (let i = 1; i <= totalPage; i++) {
+		if (page === i) {
+			// 推入粗體帶有下劃綫的頁數
+			pageNumberList.push("<b><u>[" + i + "]</u></b>");
+		} else {
+			// 推入普通頁數
+			pageNumberList.push("" + i);
+		}
+	}
+	// pageNumberList此時：totalPage為6，page為1的時候：['<b><u>1</u></b>','2','3','4','5','6']
+
+	// 截斷處理
+	if (pageNumberList.length <= 9) {
+		result = pageNumberList
+	} else {
+		if (page <= 5) {
+			// 截掉後部分
+			result = pageNumberList.slice(0, 9)
+		} else if (page >= totalPage - 4) {
+			// 截掉前部分
+			result = pageNumberList.slice(totalPage - 9);
+		} else {
+			// 截取中間
+			result = pageNumberList.slice(page - 4 - 1, page + 4);
+		}
+	}
+	return result.join("    ");;
 }
 
 module.exports = {
